4f485b774f3e8425cda0c6a5cb5b98608f73ed42
RobertDickie182 <robertdanieldickie@gmail.com>
2023-10-04 18:16:19 +0100

    drink tests passing

--- src/test/java/DrinkTest.java
+++ src/test/java/DrinkTest.java
@@ -1,2 +1,20 @@
+import Products.Drink;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DrinkTest {
+
+    Drink drink;
+
+    @Before
+    public void before(){
+        drink = new Drink("Cola");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Cola", drink.getName());
+    }
 }
