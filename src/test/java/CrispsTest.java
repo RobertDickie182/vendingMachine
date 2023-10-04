737d76396b2dd260fde9a3da2b54168cfdb01b8b
RobertDickie182 <robertdanieldickie@gmail.com>
2023-10-04 18:15:30 +0100

    crisps tests passing

--- src/test/java/CrispsTest.java
+++ src/test/java/CrispsTest.java
@@ -1,2 +1,26 @@
+import Products.Crisps;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CrispsTest {
+
+    Crisps crisps;
+
+    @Before
+    public void before(){
+        crisps = new Crisps("Walkers", "Cheese and Onion");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Walkers", crisps.getName());
+    }
+
+
+    @Test
+    public void hasFlavour(){
+        assertEquals("Cheese and Onion", crisps.getFlavour());
+    }
 }
